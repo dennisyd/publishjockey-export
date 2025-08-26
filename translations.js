6993669a70eca494941685f981509faec53936b2
dennisyd <31513813+dennisyd@users.noreply.github.com>
2025-08-26 16:59:00 -0400

    FAQ update, menu update, and TOC update

--- translations.js
+++ translations.js
@@ -48,7 +48,22 @@ const tocTranslations = {
   'xh': 'Iindeksa',
   'yo': 'At\u1ECDka',  // ọ
   'vi': 'M\u1EE5c L\u1EE5c',  // ụ, ụ
-  'zu': 'Isiqephu'
+  'zu': 'Isiqephu',
+  
+  // Missing Indic Languages
+  'bn': '\u09AC\u09BF\u09B7\u09AF\u09BC \u09A4\u09BE\u09B2\u09BF\u0995\u09BE',  // বিষয় তালিকা
+  'gu': '\u0AB5\u0ABF\u0AB7\u0AAF \u0AAF\u0ABE\u0AA6\u0AC0',  // વિષય યાદી
+  'te': '\u0C35\u0C3F\u0C37\u0C2F \u0C38\u0C42\u0C1A\u0C3F',  // విషయ సూచి
+  'kn': '\u0CB5\u0CBF\u0CB7\u0CAF \u0CB8\u0CC2\u0C9A\u0CBF',  // ವಿಷಯ ಸೂಚಿ
+  'ml': '\u0D35\u0D3F\u0D37\u0D2F \u0D38\u0D42\u0D1A\u0D3F\u0D15',  // വിഷയ സൂചിക
+  'pa': '\u0A35\u0A3F\u0A38\u0A3C\u0A47 \u0A26\u0A40 \u0A38\u0A42\u0A1A\u0A40',  // ਵਿਸ਼ੇ ਦੀ ਸੂਚੀ
+  'or': '\u0B2C\u0B3F\u0B37\u0B2F \u0B24\u0B3E\u0B32\u0B3F\u0B15\u0B3E',  // ବିଷୟ ତାଲିକା
+  
+  // Missing African Languages  
+  'sw': 'Jedwali la Yaliyomo',
+  
+  // Missing Asian Language
+  'tl': 'Talaan ng mga Nilalaman'
 };
 
 /**
